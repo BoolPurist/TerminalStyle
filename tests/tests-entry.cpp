0f97c9f8131a1168bc06d75b1221c6d9dc6c01bc
BoolPurist <fnxa@posteo.de>
2022-03-26 13:13:44 +0100

    Set-up: Integrated catch2 as test framework.

---
+++ tests/tests-entry.cpp
@@ -0,0 +1,6 @@
+//
+// Created by nicegraphic on 3/26/22.
+//
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
---
+++ tests/tests-fgColors.cpp
@@ -0,0 +1,11 @@
+//
+// Created by nicegraphic on 3/26/22.
+//
+
+#include <catch2/catch.hpp>
+#include <terminal_style/TStyler.hpp>
+
+TEST_CASE("Testing")
+{
+  REQUIRE(2 == 2);
+}
